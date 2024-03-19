5dc785f2ebd45b3bb4bb46bff47bfa92e5115a76
Kolechaaaaaaaaaaaaaa <105221104+Kolechaaaaaaaaaaaaaa@users.noreply.github.com>
2024-03-19 08:37:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 
-for( let i = 0)
+for( let i = 0; i < botoes.length; i++){
+  botoes[i].onclick = funcion(){
+    for (let j = 0; j< botoes.length; j++){
+      botoes[j].classList.remove("ativo");
+    }
+    botoes[i].classList.add("ativo")
+  }
+}
